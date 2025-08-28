9ad40042f4a8801286b62d811b219bbc4dae6b2e
Candela Naranjo <candenaranjo05@gmail.com>
2025-08-28 17:23:46 -0300

    HOLA

--- src/ejercicio4/grupo14/Ejercicio4Grupo14.java
+++ src/ejercicio4/grupo14/Ejercicio4Grupo14.java
@@ -16,6 +16,7 @@ public class Ejercicio4Grupo14 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("HOLA SOY CANDE");
     }
     
 }
